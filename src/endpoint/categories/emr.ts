adfcccd2617cccec1e32d0e131575d511d52abc6
ciniiia <cinia@blocksphere.id>
2022-06-02 13:53:14 +0700

    fix: declaration of parameter types

--- src/endpoint/categories/emr.ts
+++ src/endpoint/categories/emr.ts
@@ -1,6 +1,6 @@
 import { apiClientRequest  } from "../index";
 
-export async function getEMRCategories(url: String, key: String, auth: Object) {
+export async function getEMRCategories(url: String, key: String, auth: any) {
   const { data } = await apiClientRequest(url, key, auth)
     .get("/emr-category")  
   return data
--- src/endpoint/categories/service.ts
+++ src/endpoint/categories/service.ts
@@ -1,6 +1,6 @@
 import { apiClientRequest  } from "../index";
 
-export async function getCategories(url: String, key: String, auth: Object) {
+export async function getCategories(url: String, key: String, auth: any) {
   const { data: data } = await apiClientRequest(url, key, auth)
     .get("/service-category")  
   return data
