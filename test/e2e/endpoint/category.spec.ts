54b176524260d55daec15f58b56d9a28b0d956a7
Agustinus Theodorus <agustinustheoo@gmail.com>
2022-06-18 22:03:18 +0700

    tests: add fixture and fix categories e2e test

--- test/e2e/endpoint/category.spec.ts
+++ test/e2e/endpoint/category.spec.ts
@@ -1,33 +1,47 @@
 import { debioApiKey, debioTestUrl } from "../config";
-import { specializationCategory, emrCategory, serviceCategory } from "../../../src/endpoint/category" 
+import { emrList } from '../../mocks/postgres/models/category/emr.mock.data';
+import { serviceList } from '../../mocks/postgres/models/category/service.mock.data';
+import { specializationList } from '../../mocks/postgres/models/category/specialization.mock.data';
+import { specializationCategory, emrCategory, serviceCategory } from "../../../src/endpoint/category" ;
 
 describe('Specialization Category Storage API integration test', () => {
-
-  it(' endpoint should return', async () => {
+  it('endpoint should return', async () => {
     const result = await specializationCategory(debioTestUrl, debioApiKey, null);
     expect(result.status).toEqual(200);
-    expect(result.data).toEqual({ status: 'ok' });
+    expect(result.data).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          category: specializationList[0].category,
+        }),
+      ]),
+    );
   })
 })
 
 describe('EMR Category Storage API integration test', () => {
-
-
-  it(' endpoint should return', async () => {
+  it('endpoint should return', async () => {
     const result = await emrCategory(debioTestUrl, debioApiKey, null);
     expect(result.status).toEqual(200);
-    expect(result.data).toEqual({ status: 'ok' });
-    expect(result[0]).toContain({ "id": 1 });
+    expect(result.data).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          category: emrList[0].category,
+        }),
+      ]),
+    );
   })
 })
 
 describe('Service Category Storage API integration test', () => {
-
-
   it(' endpoint should return', async () => {
     const result = await serviceCategory(debioTestUrl, debioApiKey, null);
     expect(result.status).toEqual(200);
-    expect(result.data).toEqual({ status: 'ok' });
-    expect(result[0]).toContain({ "id": 1 });
+    expect(result.data).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          service_categories: serviceList[0].service_categories,
+        }),
+      ]),
+    );
   })
 })
